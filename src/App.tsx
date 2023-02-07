f0371daf57405af2e00808a043bb28193e952a90
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-02-08 01:44:39 +0900

    Book Component Image

--- src/App.tsx
+++ src/App.tsx
@@ -34,11 +34,14 @@ function App() {
 
 
 const Book = ({_book} : {_book : IBook}) => {
-  const [book, setBook] = useState<IBook>(_book);  
+  const [book, setBook] = useState<IBook>({..._book});  
   const [page, setPage] = useState<number>(0);
+  const nextPage = async () => {
+    setPage(page+1);
+  }
   return(
     <div className='book'>
-      <img src={book.story[page].source}/>
+      <img src={book.story[page].source} onClick={nextPage}/>
     </div>
   );
 }
@@ -60,12 +63,93 @@ const BookList = ({selectBook} : {selectBook : Function}) => {
   const data : IBook[] = [
     {
       id : 1,
-      subject : "우리는 돌멩이",
+      subject : "고집센 도토리",
       thumbnailURL : "/source/1/thum.png",
       story : [
         {
           source : "/source/1/page(1).png"
-        }
+        },
+        {
+          source : "/source/1/page(2).png"
+        },
+        {
+          source : "/source/1/page(3).png"
+        },
+        {
+          source : "/source/1/page(4).png"
+        },
+        {
+          source : "/source/1/page(5).png"
+        },
+        {
+          source : "/source/1/page(6).png"
+        },
+        {
+          source : "/source/1/page(7).png"
+        },
+        {
+          source : "/source/1/page(8).png"
+        },
+        {
+          source : "/source/1/page(9).png"
+        },
+        {
+          source : "/source/1/page(10).png"
+        },
+        {
+          source : "/source/1/page(11).png"
+        },
+        {
+          source : "/source/1/page(12).png"
+        },
+        {
+          source : "/source/1/page(13).png"
+        },
+        {
+          source : "/source/1/page(14).png"
+        },
+        {
+          source : "/source/1/page(15).png"
+        },
+        {
+          source : "/source/1/page(16).png"
+        },
+        {
+          source : "/source/1/page(17).png"
+        },
+        {
+          source : "/source/1/page(18).png"
+        },
+        {
+          source : "/source/1/page(19).png"
+        },
+        {
+          source : "/source/1/page(20).png"
+        },
+        {
+          source : "/source/1/page(21).png"
+        },
+        {
+          source : "/source/1/page(22).png"
+        },
+        {
+          source : "/source/1/page(23).png"
+        },
+        {
+          source : "/source/1/page(24).png"
+        },
+        {
+          source : "/source/1/page(25).png"
+        },
+        {
+          source : "/source/1/page(26).png"
+        },
+        {
+          source : "/source/1/page(27).png"
+        },
+        {
+          source : "/source/1/page(28).png"
+        }        
       ]
     },
     
